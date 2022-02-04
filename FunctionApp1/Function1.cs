e57adfece3d800c8252f692cd0c3af9a75a78ae2
Tony Wang <pwang009@gmail.com>
2022-02-03 16:51:30 -0800

    writing to db works again

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -16,39 +16,6 @@ namespace FunctionApp1
             log.LogInformation($"message inserted at: {queueItem.InsertionTime}");
             log.LogInformation($"C# Queue trigger function processed: {queueItem.AsString}");
             await Utils<Product>.SaveMessage(log, queueItem.AsString, queueItem.Id);
-
-            //var product = JsonSerializer.Deserialize<Product>(queueItem.AsString);
-            //log.LogInformation($"Product Name: {product.Name}");
-
-            //var str = Environment.GetEnvironmentVariable("sqlDBConnection");
-
-            //using (SqlConnection conn = new SqlConnection(str))
-            //{
-            //    conn.Open();
-            //    //var text = $"INSERT INTO dbo.message (message_id, Company, Name, Price) VALUES" +
-            //    //" ({queueItem.Id}, {product.Company}, {product.Name}, {product.Price});";
-
-            //    //var sqlcommand = "INSERT INTO message (message_id, Company, Name) VALUES (@message_id, @Company, @Name)";
-            //    var sqlcommand = "INSERT INTO message (message_id, Company, Name, Price) VALUES (@message_id, @Company, @Name, @Price)";
-
-            //    using (SqlCommand cmd = new SqlCommand(sqlcommand, conn))
-            //    {
-            //        // Execute the command and log the # rows affected.
-            //        cmd.Parameters.AddWithValue("@message_id", queueItem.Id);
-            //        cmd.Parameters.AddWithValue("@Company", product.Company);
-            //        cmd.Parameters.AddWithValue("@Name", product.Name);
-            //        cmd.Parameters.AddWithValue("@Price", product.Price);
-            //        //cmd.Parameters.AddWithValue("@message_id", queueItem.Id);
-            //        try
-            //        {
-            //            var rows = await cmd.ExecuteNonQueryAsync();
-            //            log.LogInformation($"{rows} rows were updated");
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            log.LogError($"Something went wrong {ex}");
-            //        }
-            //    }
         }
     }
 }
