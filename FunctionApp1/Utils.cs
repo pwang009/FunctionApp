4ebecad751c46b205b39905e840489cf87e51ddd
Tony Wang <pwang009@gmail.com>
2022-02-03 16:50:57 -0800

    writing db works

---
+++ FunctionApp1/Utils.cs
@@ -0,0 +1,48 @@
+﻿using FunctionApp1.Model;
+using System;
+using System.Data.SqlClient;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace FunctionApp1
+{
+    public class Utils<T> where T : class
+    {
+        public static async Task SaveMessage(ILogger log, string jsonString, string message_id)
+        {
+            var product = JsonSerializer.Deserialize<Product>(jsonString);
+            var str = Environment.GetEnvironmentVariable("sqlDBConnection");
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                //var text = $"INSERT INTO dbo.message (message_id, Company, Name, Price) VALUES" +
+                //" ({queueItem.Id}, {product.Company}, {product.Name}, {product.Price});";
+
+                //var sqlcommand = "INSERT INTO message (message_id, Company, Name) VALUES (@message_id, @Company, @Name)";
+                var sqlcommand = "INSERT INTO message (message_id, Company, Name, Price) VALUES (@message_id, @Company, @Name, @Price)";
+
+                using (SqlCommand cmd = new SqlCommand(sqlcommand, conn))
+                {
+                    // Execute the command and log the # rows affected.
+                    cmd.Parameters.AddWithValue("@message_id", message_id);
+                    cmd.Parameters.AddWithValue("@Company", product.Company);
+                    cmd.Parameters.AddWithValue("@Name", product.Name);
+                    cmd.Parameters.AddWithValue("@Price", product.Price);
+                    //cmd.Parameters.AddWithValue("@message_id", queueItem.Id);
+                    try
+                    {
+                        var rows = await cmd.ExecuteNonQueryAsync();
+                        log.LogInformation($"{rows} rows were updated");
+                    }
+                    catch (Exception ex)
+                    {
+                        log.LogError($"Something went wrong {ex}");
+                    }
+                }
+            }
+        }
+    }
+}
+
